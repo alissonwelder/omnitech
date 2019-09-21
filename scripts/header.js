8439cfc23dfe4ba106a983ae0731ee6a6068a38c
Alisson Welder <alissonwelders@gmail.com>
2019-09-20 22:33:19 -0300

    alteração

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,7 @@
 const $header = document.querySelector('header')
 const $logo = document.querySelectorAll('.logo')[0]
 const $navBar = document.querySelectorAll('.nav-bar')[0]
+const $menu = document.querySelectorAll('.menu')[0]
 window.addEventListener('scroll',toggleHeader,false)
 
 function toggleHeader(){
@@ -12,6 +13,9 @@ function toggleHeader(){
         $logo.classList.add('min-logo')
         $navBar.classList.remove('max-nav')
         $navBar.classList.add('min-nav')
+        $menu.firstElementChild.classList.remove('max-hamburger')
+        $menu.firstElementChild.classList.add('min-hamburger')
+
 
     }else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')){
         $header.classList.remove('min-header')
@@ -20,9 +24,30 @@ function toggleHeader(){
         $logo.classList.add('max-logo')
         $logo.classList.remove('min-logo')
         $navBar.classList.add('max-nav')
-        $navBar.classList.remove('min-nav') 
+        $navBar.classList.remove('min-nav')
+        $menu.firstElementChild.classList.add('max-hamburger')
+        $menu.firstElementChild.classList.remove('min-hamburger') 
     }
 }
 
+$menu.addEventListener('click',toggleMenu, false)
+var isOpen = false
+function toggleMenu(){
+    if (!isOpen){
+        $navBar.classList.add('menu-opened')
+        $menu.firstElementChild.classList.add('close-btn')
+        isOpen = true
+    }else{
+        $navBar.classList.remove('menu-opened')
+        $menu.firstElementChild.classList.remove('close-btn')
+        isOpen = false
+    }
+}
+$navBar.addEventListener('click',navClick,false)
+function navClick(evt){
+    if (evt.target.tagName == 'A'){
+        toggleMenu();
+    }
+}
 
 
